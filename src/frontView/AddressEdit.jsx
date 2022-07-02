f6f8090d175ad5ca3a8c02edce5fc2880a3b9beb
himanipahuja-01 <himanipahuja22@gmail.com>
2022-07-02 14:58:10 +0530

    newAddressEdit

--- src/frontView/AddressEdit.jsx
+++ src/frontView/AddressEdit.jsx
@@ -1,89 +1,49 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
-import { getAddress } from "../Actions/index";
+import { fetchLoginUser } from "../Actions/index";
+import axios from "axios";
 
-function AddressEdit({ item, index }) {
-  // const [data, setData] = useState({});
-
+function AddressEdit({ item, userData, fetchLoginUser }) {
   const [data, setData] = useState(item);
-
-  // useEffect(()=>{
-  //   setData(editData);
-  // },[])
-
-  console.log(data)
-  // let {id} = useParams();
-
-  // console.log(index);
-  console.log(item.id);
-  // console.log(editData);
+  fetchLoginUser();
+  
+const[user, setUser] = useState(userData)
+console.log(user);
 
   const handlechange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
 
-    // console.log(name)
-    // console.log(value)
-
-    setData((editData) => ({
-      ...editData,
+    setData((prev) => ({
+      ...prev,
       [name]: value,
     }));
   };
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   address.push(addressData);
-  //   var response = await axios({
-  //     method: "patch",
-  //     url: "http://localhost:5000/user/1",
-  //     data: {
-  //       addressData: address,
-  //     },
-  //   });
-  //   alert("Address saved");
-  // };
-
-  const getUserdetails = async () => {
-    // const response = await fetch(`http://localhost:5000/user/${id}`, {
-    //   method: "GET",
-    // });
-    // console.log(response);
-    // if (response.ok) {
-    //   const product = await response.json();
-    //   // console.log(user);
-    //   setData(product);
-    // }
-  };
-
-  const putUserdetails = async (data) => {
-    const methods = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-    const response = await fetch(
-      `http://localhost:5000/user/${item.id}`,
-      methods
-    );
-
-    if (response.ok) {
-      alert("done");
-    }
-  };
-
-  // useEffect(() => {
-  //   getUserdetails();
-  // }, []);
-
-  const onsave = (e) => {
+  const onsave = async (e) => {
     e.preventDefault();
-    // putUserdetails(data);
+    
+    setUser((prev)=>({
+      ...prev,
+      addressData: [...prev.addressData, data]
+    }))
+
+    console.log(userData)
+    var response = await axios({
+      method: "PUT",
+      url: "http://localhost:5000/user/1",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: {
+        user: user,
+      },
+    });
+    alert("Address saved");
+
   };
+  console.log(user)
 
   return (
     <div>
@@ -104,94 +64,93 @@ function AddressEdit({ item, index }) {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalLabel">
-              EDIT ADDRESS
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <form onSubmit={onsave} className="form p-5 shadow">
-              <div className="form-group mt-3">
-                <label className="form-label fw-bold">Contact Details</label>
-                <input
-                  name="contact"
-                  type="text"
-                  placeholder="Name"
-                  className="form-control"
-                  value={data.contact}
-                  onChange={handlechange}
-                />
-                <input
-                  name="mobile"
-                  type="text"
-                  placeholder="Mobile no"
-                  className="form-control mt-2"
-                  value={data.mobile}
-                  onChange={handlechange}
-                />
-              </div>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                EDIT ADDRESS
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onsave} className="form p-5 shadow">
+                <div className="form-group mt-3">
+                  <label className="form-label fw-bold">Contact Details</label>
+                  <input
+                    name="contact"
+                    type="text"
+                    placeholder="Name"
+                    className="form-control"
+                    value={data.contact}
+                    onChange={handlechange}
+                  />
+                  <input
+                    name="mobile"
+                    type="text"
+                    placeholder="Mobile no"
+                    className="form-control mt-2"
+                    value={data.mobile}
+                    onChange={handlechange}
+                  />
+                </div>
 
-              <div className="form-group mt-3">
-                <label className="form-label fw-bold">Address</label>
-                <input
-                  name="pincode"
-                  type="text"
-                  placeholder="Pincode"
-                  className="form-control mt-2"
-                  value={data.pincode}
-                  onChange={handlechange}
-                />
+                <div className="form-group mt-3">
+                  <label className="form-label fw-bold">Address</label>
+                  <input
+                    name="pincode"
+                    type="text"
+                    placeholder="Pincode"
+                    className="form-control mt-2"
+                    value={data.pincode}
+                    onChange={handlechange}
+                  />
 
-                <input
-                  name="address"
-                  type="text"
-                  placeholder="Address(House no, Building street area)"
-                  className="form-control mt-2"
-                  value={data.address}
-                  onChange={handlechange}
-                />
+                  <input
+                    name="address"
+                    type="text"
+                    placeholder="Address(House no, Building street area)"
+                    className="form-control mt-2"
+                    value={data.address}
+                    onChange={handlechange}
+                  />
 
-                <input
-                  name="location"
-                  type="text"
-                  placeholder="Location"
-                  className="form-control mt-2"
-                  value={data.location}
-                  onChange={handlechange}
-                />
-              </div>
+                  <input
+                    name="location"
+                    type="text"
+                    placeholder="Location"
+                    className="form-control mt-2"
+                    value={data.location}
+                    onChange={handlechange}
+                  />
+                </div>
 
-              <div className="buttons mt-3">
-                <button
-                  type="submit"
-                  // disabled={submitting || pristine}
-                  className="btn btn-primary"
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
+                <div className="buttons mt-3">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    // </div>
   );
 }
 
 // export default AddressEdit;
-function mapDispatchToProps(dispatch) {
+const mapStateToProps = (state) => {
+  //  console.log(state)
   return {
-    getAddress: (item) => dispatch(getAddress(item)),
+    userData: state.userData,
   };
-}
+};
 
-export default connect(mapDispatchToProps, { getAddress })(AddressEdit);
+export default connect(mapStateToProps, {
+  fetchLoginUser,
+})(AddressEdit);
--- src/frontView/AddressForm.jsx
+++ src/frontView/AddressForm.jsx
@@ -1,19 +1,15 @@
 import React from "react";
-// import { Form, Field } from "react-final-form";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { connect } from "react-redux";
 import { useState } from "react";
 import { fetchLoginUser, AddCart, getAddress } from "../Actions/index";
 import AddressEdit from "./AddressEdit";
-// import { FiEdit } from 'react-icons/fi';
-// import { useEffect } from "react";
 
 var address = [];
 
 function AddressForm(props) {
   console.log(props.items.Cart);
-  //   console.log(props.userData[0].id);
 
   const [addressData, setAddressData] = useState({
     id: "",
@@ -23,15 +19,10 @@ function AddressForm(props) {
     address: "",
     location: "",
   });
-  const[editData, setEditData] = useState()
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-
-    // console.log(name)
-    // console.log(value)
-
     setAddressData((prev) => ({
       ...prev,
       [name]: value,
@@ -39,11 +30,12 @@ function AddressForm(props) {
     // console.log(addressData)
   };
 
+  console.log(addressData);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // console.log(addressData);
+
     address.push(addressData);
-    // console.log(address);
     var response = await axios({
       method: "patch",
       url: "http://localhost:5000/user/1",
@@ -51,11 +43,8 @@ function AddressForm(props) {
         addressData: address,
       },
     });
-    //   props.AddCart(props.productDetails);
     alert("Address saved");
   };
-
-  //   console.log(props.userData[0].addressData);
 
   let ListCart = [];
   let TotalCart = 0;
@@ -65,22 +54,18 @@ function AddressForm(props) {
   });
 
   const [show, setShow] = useState("none");
-  // const [padding, setpadding] = useState('0px');
 
   const styling = {
     display: show,
     // marginBottom: `${padding}`
   };
 
-  const styled = () => {
+  const styled = (id, index) => {
     setShow("flex");
-    // setpadding('300px');
   };
 
   var date = new Date();
   date.setDate(date.getDate() + 3);
-  //   console.log(date);
-  // console.log(padding)
 
   return (
     <div>
@@ -155,11 +140,7 @@ function AddressForm(props) {
                   </div>
 
                   <div className="buttons mt-3">
-                    <button
-                      type="submit"
-                      // disabled={submitting || pristine}
-                      className="btn btn-primary"
-                    >
+                    <button type="submit" className="btn btn-primary">
                       Submit
                     </button>
                   </div>
@@ -169,10 +150,7 @@ function AddressForm(props) {
           </div>
         </div>
 
-        <section
-          className="h-custom margin"
-          // style={{backgroundColor: 'blue'}}
-        >
+        <section className="h-custom margin">
           <div className="row g-3 margin">
             <div className="col-lg-7">
               <h5 className="mb-3">
@@ -184,11 +162,6 @@ function AddressForm(props) {
               <hr />
 
               <div className="d-flex justify-content-between align-items-center mb-4">
-                {/* <div>
-                    <p className="mb-1">Shopping cart</p>
-                    <p className="mb-0">You have 4 items in your cart</p>
-                  </div> */}
-
                 <div
                   type="button"
                   className="btn border button"
@@ -204,7 +177,7 @@ function AddressForm(props) {
                   <div
                     className="card mb-3 w-100 heights"
                     onClick={() => {
-                      styled();
+                      styled(item.id, key);
                     }}
                   >
                     <div className="card-body">
@@ -218,14 +191,7 @@ function AddressForm(props) {
                               id="flexRadioDefault1"
                             />
                           </div>
-                          <div>
-                            {/* <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                            className="img-fluid rounded-3"
-                            alt="Shopping item"
-                            style={{ width: "65px" }}
-                          /> */}
-                          </div>
+                          <div></div>
                           <div className="ms-3">
                             <h5>{item.contact}</h5>
                             <p className="small mb-0">{item.mobile}</p>
@@ -234,66 +200,22 @@ function AddressForm(props) {
                             </h5>
                           </div>
                         </div>
-                        {/* <div className="d-flex flex-row align-items-center"> */}
-                        {/* <div style={{ width: "50px" }}>
-                        </div>
-                        <div style={{ width: "80px" }}>
-                          <h5 className="mb-0">{item.pincode}</h5>
-                        </div>
-                        <a
-                          href="#!"
-                          // style="color: #cecece;"
-                        > */}
-
-                        {/* </a> */}
-                        {/* </div> */}
                       </div>
-                      {/* <div className="d-flex justify-content-between">
-                      <i className="fas fa-trash-alt"></i>
-                      <FiEdit/>
-                      </div> */}
+
                       <div
                         className="col-lg-5 col-md-6 ms-3 mt-3"
                         style={styling}
                       >
-                        {/* <p>Size: M</p> */}
                         <button
                           type="button"
                           className="btn border button ms-3 px-2"
                           data-mdb-toggle="tooltip"
                           title="Remove item"
                         >
-                          {/* <i className="fas fa-trash"></i> */}
                           Remove
                         </button>
-                        {/* <button
-                                    type="button"
-                                    className="btn border button p-1 ms-3 px-2"
-                                    data-mdb-toggle="tooltip"
-                                    title="Move to the wish list"
-                                  > */}
-                        {/* <FiEdit className="fs-6 p-0"/> */}
-                        {/* Edit
-                                  </button> */}
-                        {/* <button
-                          type="button"
-                          className="btn border button ms-3 px-2"
-                          data-bs-toggle="modal"
-                          data-bs-target={`exampleModal${item.id}`}
-                          data-bs-whatever="@edit"
-                          // to={`/AddtoCart/addressform/Addressedit/${item.id}`}
-                        >
-                          Edit
-                        </button> */}
-                        {/* <div
-                          className="modal fade"
-                          id={`exampleModal${item.id}`}
-                          tabIndex="-1"
-                          aria-labelledby="exampleModalLabel"
-                          aria-hidden="true"
-                        > */}
-                          <AddressEdit index={key} item={item}/>
-                        {/* </div> */}
+
+                        <AddressEdit index={key} item={item} />
                       </div>
                     </div>
                   </div>
@@ -303,11 +225,9 @@ function AddressForm(props) {
 
             <div className="col-lg-4 ms-4">
               <div className="card h-auto w-100">
-                {/* <div className="d-flex justify-content-between"> */}
                 <h6 className="ms-3 mt-3">DELIVERY ESTIMATES</h6>
 
                 <div className="card-body p-0">
-                  {/* <div className="d-flex flex-row align-items-center"> */}
                   {ListCart.map((item, key) => {
                     console.log(item);
                     return (
@@ -324,11 +244,9 @@ function AddressForm(props) {
                             Estimated delivery at {date.toLocaleDateString()}
                           </p>
                         </div>
-                        {/* <hr /> */}
                       </div>
                     );
                   })}
-                  {/* </div> */}
                 </div>
 
                 <div className="bg-white">
@@ -337,10 +255,6 @@ function AddressForm(props) {
                   </div>
                   <div className="card-body">
                     <ul className="list-group list-group-flush">
-                      {/* <li className="list-group-item d-flex justify-content-between align-items-center px-0">
-                        Shipping
-                        <span>Gratis</span>
-                      </li> */}
                       <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0">
                         <div>
                           <p>MRP</p>
@@ -384,124 +298,8 @@ function AddressForm(props) {
                     </Link>
                   </div>
                 </div>
-
-                {/* </div> */}
               </div>
             </div>
-
-            {/* <div className="col-lg-3">
-                <div className="card bg-primary text-white rounded-3 h-100 w-100">
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between align-items-center mb-4">
-                      <h5 className="mb-0">Card details</h5> */}
-            {/* <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
-                        className="img-fluid rounded-3"
-                        style={{ width: "45px" }}
-                        alt="Avatar"
-                      /> */}
-            {/* </div> */}
-
-            {/* <p className="small mb-2">Card type</p>
-                    <Link to="/" type="submit" className="text-white">
-                      <i className="fab fa-cc-mastercard fa-2x me-2"></i>
-                    </Link>
-                    <Link to="/" type="submit" className="text-white">
-                      <i className="fab fa-cc-visa fa-2x me-2"></i>
-                    </Link>
-                    <Link to="/" type="submit" className="text-white">
-                      <i className="fab fa-cc-amex fa-2x me-2"></i>
-                    </Link>
-                    <Link to="/" type="submit" className="text-white">
-                      <i className="fab fa-cc-paypal fa-2x"></i>
-                    </Link> */}
-
-            {/* <form className="mt-4">
-                      <div className="form-outline form-white mb-4">
-                        <input
-                          type="text"
-                          className="form-control form-control-lg"
-                          size="17"
-                          placeholder="Cardholder's Name"
-                        />
-                        <label className="form-label">Cardholder's Name</label>
-                      </div> */}
-            {/* 
-                      <div className="form-outline form-white mb-4">
-                        <input
-                          type="text"
-                          className="form-control form-control-lg"
-                          siez="17"
-                          placeholder="1234 5678 9012 3457"
-                          minLength="19" */}
-            {/* maxLength="19"
-                        />
-                        <label className="form-label">Card Number</label>
-                      </div>
-
-                      <div className="row mb-4">
-                        <div className="col-lg-6">
-                          <div className="form-outline form-white"> */}
-            {/* <input
-                              type="text"
-                              className="form-control form-control-lg"
-                              placeholder="MM/YYYY"
-                              size="7"
-                              minLength="7"
-                              maxLength="7"
-                            /> */}
-            {/* <label className="form-label">Expiration</label>
-                          </div>
-                        </div>
-                        <div className="col-md-6">
-                          <div className="form-outline form-white">
-                            <input
-                              type="password"
-                              id="typeText"
-                              className="form-control form-control-lg"
-                              placeholder="&#9679;&#9679;&#9679;"
-                              size="1"
-                              minLength="3"
-                              maxLength="3"
-                            />
-                            <label className="form-label">Cvv</label>
-                          </div> */}
-            {/* </div>
-                      </div>
-                    </form>
-
-                    <hr className="my-4" />
-
-                    <div className="d-flex justify-content-between">
-                      <p className="mb-2">Subtotal</p>
-                      <p className="mb-2">$4798.00</p>
-                    </div> */}
-            {/* 
-                    <div className="d-flex justify-content-between">
-                      <p className="mb-2">Shipping</p>
-                      <p className="mb-2">$20.00</p>
-                    </div>
-
-                    <div className="d-flex justify-content-between mb-4">
-                      <p className="mb-2">Total(Incl. taxes)</p>
-                      <p className="mb-2">$4818.00</p>
-                    </div> */}
-
-            {/* <button
-                      type="button"
-                      className="btn btn-info btn-block btn-lg"
-                    >
-                      <div className="d-flex justify-content-between">
-                        <span>$4818.00</span>
-                        <span>
-                          Checkout{" "}
-                          <i className="fas fa-long-arrow-alt-right ms-2"></i>
-                        </span>
-                      </div>
-                    </button> */}
-            {/* </div> */}
-            {/* </div> */}
-            {/* </div> */}
           </div>
         </section>
       </div>
